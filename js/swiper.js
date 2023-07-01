df5facf9d5f4d5fa4605ae072210cf2bede993d1
YumiN <yuminishi0309@gmail.com>
2023-07-01 23:53:55 +0900

    fix: Scrolling in boxes turn off, update: Tidy up the display of swiper slides

--- js/swiper.js
+++ js/swiper.js
@@ -3,44 +3,16 @@ const swiperWorks = new Swiper ('.swiper-works', {
     direction: 'horizontal',
     loop: false,
     effect: 'slide',
-    // grabCursor: true,
-
-    slidesPerView: 1.3,
+    slidesPerView: 2,
         breakpoints: {
             768: {
-                slidesPerView: 1.8,
+                slidesPerView: 3,
             },
             1024: {
                 slidesPerView: 4,
             }
         },
-
     spaceBetween: 20,
-        // breakpoints: {
-        //     768: {
-        //         spaceBetween: 60,
-        //     },
-        //     1024: {
-        //         spaceBetween: 60,
-        //     }
-        // },
-
-    // slidesOffsetBefore: 20,
-        // breakpoints: {
-        //     768: {
-        //         slidesOffsetBefore: 80,
-
-        //     },
-        //     1024: {
-        //         spaceBetween: 160,
-        //     }
-        // },
-
-
-    // autoplay : {
-    //     delay: 300,
-    // }
-    // speed: 6000,
     pagination: {
         el: '.swiper-works__pagination',
         type: 'bullets',
@@ -50,6 +22,4 @@ const swiperWorks = new Swiper ('.swiper-works', {
         prevEl: '.swiper-works__prev',
         nextEl: '.swiper-works__next'
     }
-    
-
 });
